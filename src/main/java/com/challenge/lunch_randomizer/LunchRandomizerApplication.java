83c2283b6055202801b159612481cdf1365b7967
ChesterECQ <chester44311@gmail.com>
2023-09-12 10:49:46 +0800

    Initial Commit

---
+++ src/main/java/com/challenge/lunch_randomizer/LunchRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.lunch_randomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/lunch_randomizer/LunchRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.lunch_randomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
