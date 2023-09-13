6261c73a037dfd91c7669c09663a3b4ab7c78aa7
Chester <chester44311@gmail.com>
2023-09-14 00:44:44 +0800

    Add cross origin annotation

--- src/main/java/com/challenge/lunch_randomizer/controller/LunchController.java
+++ src/main/java/com/challenge/lunch_randomizer/controller/LunchController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +20,7 @@ import java.util.Map;
 
 @Log4j2
 @RestController
+@CrossOrigin
 @RequestMapping("lunch")
 public class LunchController {
     @Autowired
